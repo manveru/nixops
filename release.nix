37dc4c6d2f1830e2f3c988c92b070da020f97ffd
Eelco Dolstra <edolstra@gmail.com>
2019-09-26 19:56:45 +0200

    Disable the none_backend test
    It has been broken for nearly 3 years and takes 2 hours to time out.

--- release.nix
+++ release.nix
@@ -1,15 +1,16 @@
 { nixpkgs ? <nixpkgs>
+, nixopsSrc ? { outPath = ./.; revCount = 0; shortRev = "abcdef"; rev = "HEAD"; }
+, officialRelease ? false
+, p ? (p: [ ])
 }:
 
 let
   pkgs = import nixpkgs { config = {}; overlays = []; };
 
 in rec {
-
   nixops = pkgs.lib.genAttrs [ "x86_64-linux" "i686-linux" "x86_64-darwin" ] (system:
     let
       pkgs = import nixpkgs { inherit system; };
       nixops = import ./default.nix { inherit pkgs; };
     in nixops);
-
 }
